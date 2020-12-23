e4670341a58ecb5027dc311dddb0d14cab9a6c44
Zaid <zaidhassanch@gmail.com>
2020-12-23 23:09:46 +0500

    working ...

--- T001_sort_understand/main.py
+++ T001_sort_understand/main.py
@@ -1,3 +1,8 @@
+"""
+di_prime: Attention aware decoder state
+uj: Energy vector (Attention Layer)
+aj: Attention mask over the input
+"""
 import random
 from typing import Tuple
 from test import computeAttn
@@ -7,7 +12,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-from train import train, evaluate, BATCH_SIZE
+from train import train, evaluate
 
 HIDDEN_SIZE = 256
 EPOCHS = 10
@@ -21,40 +26,69 @@ class Attention(nn.Module):
     self.V =  nn.Linear(units, 1, bias=False)
     return
 
-  def forward(self, 
-              encoder_out: torch.Tensor, 
+  def forward(self,
+              encoder_out: torch.Tensor,
               decoder_hidden: torch.Tensor):
+    # encoder_out: (BATCH, ARRAY_LEN, HIDDEN_SIZE)
+    # decoder_hidden: (BATCH, HIDDEN_SIZE)
+
+    # Add time axis to decoder hidden state
+    # in order to make operations compatible with encoder_out
+    # decoder_hidden_time: (BATCH, 1, HIDDEN_SIZE)
     decoder_hidden_time = decoder_hidden.unsqueeze(1)
+    # Note: we can add the both linear outputs thanks to broadcasting
     uj = self.W1(encoder_out) + self.W2(decoder_hidden_time)
     uj = torch.tanh(uj)
+
+    # uj: (BATCH, ARRAY_LEN, 1)
     uj = self.V(uj)
+
+    # Attention mask over inputs
+    # aj: (BATCH, ARRAY_LEN, 1)
     aj = F.softmax(uj, dim=1)
+
+    # di_prime: (BATCH, HIDDEN_SIZE)
     di_prime = aj * encoder_out
     di_prime = di_prime.sum(1)
+
     uj = uj.squeeze(-1)
+
     return di_prime, uj
 
+
 class Decoder(nn.Module):
-  def __init__(self, 
+  def __init__(self,
                hidden_size: int,
                attention_units: int = 10):
     super(Decoder, self).__init__()
     self.lstm = nn.LSTM(hidden_size + 1, hidden_size, batch_first=True)
     self.attention = Attention(hidden_size, attention_units)
-    self.hs = torch.zeros(BATCH_SIZE, HIDDEN_SIZE, dtype=torch.float)
-
     return
 
-  def forward(self, 
-              x: torch.Tensor, 
-              hidden: Tuple[torch.Tensor], 
+  def forward(self,
+              x: torch.Tensor,
+              hidden: Tuple[torch.Tensor],
               encoder_out: torch.Tensor):
+    # x: (BATCH, 1, 1)
+    # hidden: (1, BATCH, HIDDEN_SIZE)
+    # encoder_out: (BATCH, ARRAY_LEN, HIDDEN_SIZE)
+    # For a better understanding about hidden shapes read: https://pytorch.org/docs/stable/nn.html#lstm
 
-    di, att_w = self.attention(encoder_out, hidden)
+    # Get hidden states (not cell states)
+    # from the first and unique LSTM layer
+    ht = hidden[0][0]  # ht: (BATCH, HIDDEN_SIZE)
+
+    # di: Attention aware hidden state -> (BATCH, HIDDEN_SIZE)
+    # att_w: Not 'softmaxed', torch will take care of it -> (BATCH, ARRAY_LEN)
+    di, att_w = self.attention(encoder_out, ht)
+
+    # Append attention aware hidden state to our input
+    # x: (BATCH, 1, 1 + HIDDEN_SIZE)
     x = torch.cat([di.unsqueeze(1), x], dim=2)
-    _, hidden = self.lstm(x)
-    # self.hs =
-    return hidden[0][0], att_w
+
+    # Generate the hidden state for next timestep
+    _, hidden = self.lstm(x, hidden)
+    return hidden, att_w
 
 
 class PointerNetwork(nn.Module):
@@ -63,19 +97,34 @@ class PointerNetwork(nn.Module):
     self.encoder = nn.LSTM(1, HIDDEN_SIZE, batch_first=True)
     self.decoder = Decoder(HIDDEN_SIZE)
 
-  def forward(self, 
-              x: torch.Tensor, 
-              y: torch.Tensor, 
+  def forward(self,
+              x: torch.Tensor,
+              y: torch.Tensor,
               teacher_force_ratio=.5):
+    # x: (BATCH_SIZE, ARRAY_LEN)
+    # y: (BATCH_SIZE, ARRAY_LEN)
 
+    # Array elements as features
+    # encoder_in: (BATCH, ARRAY_LEN, 1)
     encoder_in = x.unsqueeze(-1).type(torch.float)
+
+    # out: (BATCH, ARRAY_LEN, HIDDEN_SIZE)
+    # hs: tuple of (NUM_LAYERS, BATCH, HIDDEN_SIZE)
     out, _ = self.encoder(encoder_in)
+
+    # Accum loss throughout timesteps
     loss = 0
 
+    # Save outputs at each timestep
+    # outputs: (ARRAY_LEN, BATCH)
     outputs = torch.zeros(out.size(1), out.size(0), dtype=torch.long)
-    
+
+    # First decoder input is always 0
+    # dec_in: (BATCH, 1, 1)
     dec_in = torch.zeros(out.size(0), 1, 1, dtype=torch.float)
-    hs = torch.zeros(out.size(0), out.size(2), dtype=torch.float)
+    # hs = (hs[0]*0, hs[1] * 0)
+    hsa = torch.zeros(1, out.size(0), out.size(2), dtype=torch.float)
+    hs = (hsa, hsa)
     for t in range(out.size(1)):
       hs, att_w = self.decoder(dec_in, hs, out)
       predictions = F.softmax(att_w, dim=1).argmax(1)
