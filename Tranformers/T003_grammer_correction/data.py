f0d572029bc2356b2c2981e8d936d99ff2c9deed
Zaid <zaidhassanch@gmail.com>
2020-12-27 22:40:02 +0500

    .

--- Tranformers/T003_grammer_correction/data.py
+++ Tranformers/T003_grammer_correction/data.py
@@ -11,7 +11,7 @@ python -m spacy download de
 """
 from torchtext.data.utils import get_tokenizer
 
-spacy_ger = spacy.load("de")
+spacy_ger = spacy.load("en")
 spacy_eng = spacy.load("en")
 
 
@@ -56,6 +56,7 @@ def getData():
     # were
     # carefully
     # worded and chosen.
+    print("===============================before ")
     train_data, valid_data, test_data = Multi30k.splits(
         exts=(".src", ".tgt"), fields=(german, english),
         # root='.data',
