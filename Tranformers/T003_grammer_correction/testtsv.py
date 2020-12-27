0ba4f807a691fd6d642c1ad5865ca0fa66bf79bc
Zaid Hassan Chaudhry <zaidhassanch@gmail.com>
2020-12-27 02:01:48 -0800

    .

--- Tranformers/T003_grammer_correction/testtsv.py
+++ Tranformers/T003_grammer_correction/testtsv.py
@@ -2,28 +2,31 @@ import spacy
 from torchtext.data import Field, BucketIterator
 
 from torchtext import data
-    
+
+#original_sentence	edited_sentence	edited	numchanges
 # create Field objects
-AUTHOR = data.Field()
-AGE = data.Field()
-TWEET = data.Field()
-LOCATION = data.Field()
+ORIGINAL_SENTENCE = data.Field()
+EDITED_SENTENCE = data.Field()
+EDITED = data.Field()
+NUMCHANGES = data.Field()
 
 # create tuples representing the columns
 fields = [
-  ('author', AUTHOR),
-  ('location', LOCATION),
-  (None, None), # ignore age column
-  ('tweet', TWEET)
+  ('original_sentence', ORIGINAL_SENTENCE),
+  ('edited_sentence', EDITED_SENTENCE),
+  ('edited', EDITED), # ignore age column
+  ('numchanges', NUMCHANGES)
 ]
 
 # load the dataset in json format
 test_ds = data.TabularDataset.splits(
-   path = '.',
-   test = 'test.tsv',
+#   path = '/data/chaudhryz/uwstudent1/2017-data/',
+   path ='.',
+   test = '2017.tsv',
    format = 'tsv',
    fields = fields,
    skip_header = True
 )
 
-print(test_ds[0].examples[0].author[0])
+print(test_ds[0].examples[1].original_sentence)
+print(dir(test_ds[0].examples[1]))
