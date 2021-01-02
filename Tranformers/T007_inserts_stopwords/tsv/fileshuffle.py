eec117dac9962ae8884739f42337e7f9ad7923d2
Zaid <zaidhassanch@gmail.com>
2021-01-02 12:28:41 +0500

    updated fileshuffle in tsv folder

--- Tranformers/T007_inserts_stopwords/tsv/fileshuffle.py
+++ Tranformers/T007_inserts_stopwords/tsv/fileshuffle.py
@@ -7,22 +7,47 @@ parser.add_argument("--infile", "-i", help="the input file")
 args = parser.parse_args()
 assert(args.infile)
 
+
 def shuffleLine(line):
-    line = "Four guys three wearing hats one not are jumping at the top of a staircase."
+    #line = "Four guys three wearing hats one not are jumping at the top of a staircase."
     line = line.replace(".", "")
+    line = removeStopWords(line)
     words = line.split()
     random.shuffle(words)
     #print(words)
     line = " ".join(words) + "."
     #print(line)
     #exit()
+
     return line+"\n"
+
+def removeStopWords(line, stop_words=["a", "an", "the", "at", "are", "were", "is", "on", "in", "with", "of", "and", "for"]):
+    #line = "Four guys three wearing hats one not are jumping at the top of a staircase."
+    #line = line.replace(".", "")
+    line = line.lower()
+    if(line[:2] == 'a '):
+        line = line[2:]
+    if(line[:3] == 'an '):
+        line = line[3:]
+    if (line[:4] == 'the '):
+        line = line[4:]
+
+
+    for s_word in stop_words:
+        line = line.replace(" " + s_word + " ", " ")
+    # words = line.split()
+    #random.shuffle(words)
+    #print(words)
+    #line = " ".join(words) + "."
+    #print(line)
+    #exit()
+    return line
 
 
 line = "Four guys three wearing hats one not are jumping at the top of a staircase."
 print(shuffleLine(line))
 # exit()
-outfile = args.infile  + "s"
+outfile = args.infile  + "nsw"
 fw = open(outfile, 'w')
 
 count = 0
