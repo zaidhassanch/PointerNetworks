4d4149a29743303d9bf6185b6f70fbbaad49c94a
Zaid <zaidhassanch@gmail.com>
2021-02-14 12:19:26 +0500

    .

--- DATA_GENERATION/T001_wordnet/main.py
+++ DATA_GENERATION/T001_wordnet/main.py
@@ -5,7 +5,7 @@ filePath = "/home/zaid/DrPascal/data/NOVELS/"
 files = os.listdir(filePath)
 
 print(files)
-outFile = open("outCombined.txt", mode='w')
+outFile = open(filePath+"processed/outCombined.txt", mode='w')
 
 for fileName in files:
     cleanFile(filePath, fileName, outFile)
