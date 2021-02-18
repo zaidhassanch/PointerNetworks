ebf71ddfeb76a04fb430da0bc4e683b996a0fd66
Zaid <zaidhassanch@gmail.com>
2021-02-18 16:09:16 +0500

    .

--- Tranformers/T003a_understanding_bucket_iterator/T006_bleu_score/bleu_compute_tsv.py
+++ Tranformers/T003a_understanding_bucket_iterator/T006_bleu_score/bleu_compute_tsv.py
@@ -25,38 +25,16 @@ targets = []
 outputs = []
 
 for i, line in enumerate(f):
-    orig = line.split("\t")
-    print(orig)
-#
-#     orig = line.strip().split(".", 1)[0]
-#     print(line.strip())
-#     print(orig)
-#     tokens_orig = [token.text.lower() for token in spacy_eng(orig)]
-#
-#     outputs.append(tokens_orig)
-#
-#     bleu = bleu_score([outputs[-1]], [targets[-1]])
-#     bleu_overall = bleu_score(outputs, targets)
-#     BleuScores.append(bleu)
-#     # if np.int(np.floor((i + 1) /5)+ 1) % 2 == 0:
-#     #     # bleu = bleu_score(outputs, targets)
-#     #     print(np.int(np.floor((i + 1) /5)+1))
-#     #     bleu = bleu_score(outputs, targets)
-#     #     BleuScores.append(bleu)
-#     #     outputs = []
-#     #     targets = []
-#
-#     if (i - 1) % 5 == 0:
-#         targ = line.strip().split(".", 1)[0]
-#         print(line.strip())
-#         print("+++++", targ)
-#
-#         tokens_targ = [token.text.lower() for token in spacy_eng(targ)]
-#
-#         targets.append([tokens_targ])
-#
-# BleuScores = np.array(BleuScores)
-# print("average = ", np.mean(BleuScores))
-# print(bleu_score(outputs, targets))
-# print(BleuScores)
-#
+    orig = line.split("\t")[0]
+    targ = line.split("\t")[1]
+    # print(orig)
+
+    tokens_orig = [token.text.lower() for token in spacy_eng(orig)]
+    tokens_targ = [token.text.lower() for token in spacy_eng(targ)]
+
+    outputs.append(tokens_orig)
+    targets.append([tokens_targ])
+
+    if i == 3:
+        break
+print(bleu_score(outputs, targets))
