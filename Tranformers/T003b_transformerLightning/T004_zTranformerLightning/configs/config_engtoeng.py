ac028ca908796263ec0176a804115b9961dbc7a2
Zaid <zaidhassanch@gmail.com>
2021-03-14 15:36:18 +0500

    adding config files

---
+++ Tranformers/T003b_transformerLightning/T004_zTranformerLightning/configs/config_engtoeng.py
@@ -0,0 +1,20 @@
+BPE_FROM_PICKLE = True
+BPE_PATH = "GCEBPE4m.model"
+
+PYTORCH_TRANSFORMER = False
+LOAD_NEW_METHOD = True
+USE_BPE = False
+MAX_LEN = 500
+
+TRAIN_SRC = '.data/multi30k/train.en'
+TRAIN_TGT = '.data/multi30k/train.en'
+VAL_SRC = '.data/multi30k/val.en'
+VAL_TGT = '.data/multi30k/val.en'
+TEST_SRC = '.data/multi30k/test2016.en'
+TEST_TGT = '.data/multi30k/test2016.en'
+
+sentences = []
+sentences.append("A horse is walking beside a boat under a bridge.")
+sentences.append("Two men are removing tree branches.")
+sentences.append("A young boy in a red life jacket is swimming in a pool.")
+sentences.append("Two kids are swinging on a playground.")
---
+++ Tranformers/T003b_transformerLightning/T004_zTranformerLightning/configs/config_eyre.py
@@ -0,0 +1,20 @@
+BPE_FROM_PICKLE = True
+BPE_PATH = "GCEBPE4m.model"
+
+PYTORCH_TRANSFORMER = False
+LOAD_NEW_METHOD = True
+USE_BPE = True
+MAX_LEN = 500
+
+TRAIN_SRC = 'tsv/sent_9_jane_eyre.src'
+TRAIN_TGT = 'tsv/sent_9_jane_eyre.tgt'
+VAL_SRC = 'tsv/sent_9_jane_eyre.src'
+VAL_TGT = 'tsv/sent_9_jane_eyre.tgt'
+TEST_SRC = 'tsv/sent_9_jane_eyre.src'
+TEST_TGT = 'tsv/sent_9_jane_eyre.tgt'
+
+sentences = []
+sentences.append("What, you is a baby after all!")
+sentences.append("You is afraid of ghosts?")
+sentences.append("But is your relatives so very poor?")
+sentences.append("Well, Jane Eyre, and is you a good child?")
