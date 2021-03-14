d94e9f852fc77a6bab0634a6f14ae41e5952c6fc
Zaid <zaidhassanch@gmail.com>
2021-03-14 18:20:18 +0500

    .

--- Tranformers/T003b_transformerLightning/T004_zTranformerLightning/tsv/tsvToParallel.py
+++ Tranformers/T003b_transformerLightning/T004_zTranformerLightning/tsv/tsvToParallel.py
@@ -11,6 +11,11 @@ count = 0
 with open(inFile, 'r') as fp:
     for line in fp:
         line = line.strip()
+        if count == 332:
+            print("===========")
+            print("line[0]:", line[0], ":line[0]")
+            print("line[1]:", line[1], ":line[1]")
+
         if line[0] == "\t":
             line[0] = ""
             line[1] = ""
@@ -19,6 +24,8 @@ with open(inFile, 'r') as fp:
         count += 1
         if count == 333:
             print("===========")
+            print("line[0]", line[0])
+            print("line[1]", line[1])
         print(count, line[0])
         fwSRC.write(line[0] + "\n")
         fwTGT.write(line[1] + "\n")
